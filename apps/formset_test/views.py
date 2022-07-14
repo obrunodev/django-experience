644a5a939e34b8f51ccfdf45f4d98363ce16c08e
Bruno R. Pianca <brunorpdev@gmail.com>
2022-07-14 01:46:31 -0300

    feat: Customers pagination added

--- apps/formset_test/views.py
+++ apps/formset_test/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from formset_test.forms import CustomerForm
@@ -8,7 +9,10 @@ from formset_test.models import Customer
 
 def customers_list(request):
     customers = Customer.objects.all()
-    context = {'customers': customers}
+    paginator = Paginator(customers, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'formset_test/pages/customers_list.html', context)
 
 
